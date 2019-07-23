96de25c8baa7346666e79b90d71df50093b0e10d
zeeshan-iqbal <link2zeshan@gmail.com>
2019-07-23 10:09:29 +0400

    Finished chapeter one

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -48,7 +48,7 @@ export default class MoviesDAO {
     Ticket: Projection
 
     Write a query that matches movies with the countries in the "countries"
-    list, but only returns the title and _id of each movie.
+    list, but only returns the title and _id of each movie.    
 
     Remember that in MongoDB, the $in operator can be used with a list to
     match one or more values of a specific field.
@@ -61,7 +61,10 @@ export default class MoviesDAO {
       // and _id. Do not put a limit in your own implementation, the limit
       // here is only included to avoid sending 46000 documents down the
       // wire.
-      cursor = await movies.find().limit(1)
+      cursor = await movies.find(
+        { countries: { $in: countries } },
+        { projection: { title: 1 } },
+      )
     } catch (e) {
       console.error(`Unable to issue find command, ${e}`)
       return []
@@ -116,7 +119,7 @@ export default class MoviesDAO {
 
     // TODO Ticket: Text and Subfield Search
     // Construct a query that will search for the chosen genre.
-    const query = {}
+    const query = { genres: { $in: searchGenre } }
     const project = {}
     const sort = DEFAULT_SORT
 
@@ -296,9 +299,9 @@ export default class MoviesDAO {
       const pipeline = [
         {
           $match: {
-            _id: ObjectId(id)
-          }
-        }
+            _id: ObjectId(id),
+          },
+        },
       ]
       return await movies.aggregate(pipeline).next()
     } catch (e) {
